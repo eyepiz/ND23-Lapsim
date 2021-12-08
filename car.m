9c6ae90003db266ead678a84355f0be5218afeff
jvasilak <jvasilak@nd.edu>
2021-12-07 19:49:59 -0500

    Car and Track Start

---
+++ car.m
@@ -0,0 +1,26 @@
+classdef car
+   properties
+      mass
+      wheel_base
+      tire
+      engine % class for the engine subsystem
+      chassis % class for the chassis subsytem
+      electrical % class for the electrical subsystem
+      brake % class for the brakes subsystem
+      drivetrain % class for the drivetrain subsystem
+      % Add all other properties to the default constructor function
+   end
+   methods
+       % Default Constructor
+       function obj = car(mass, wb, tire, eng, brake, d_t)
+          obj.mass = mass;
+          obj.wheel_base = wb;
+          obj.tire = tire;
+          obj.engine = eng;
+          obj.brake = brake;
+          obj.drivetrain = d_t;
+       end
+      %% Functions Needed:
+      
+   end
+end
---
+++ track.m
@@ -0,0 +1,25 @@
+% The purpose of this file is to provide the functions needed to build a track
+% for running our lap simulations on.
+
+% Functions needed include functions to build straights and turns, as well
+% as the entire courses themselves, built using the straights and turns
+% functions
+function straight = build_straight(length)
+
+end
+
+function acceleration = build_acc(length)
+    acceleration = build_straight(length);
+end
+
+function endurance = build_endurance()
+
+end
+
+function skidpad = build_skidpad()
+
+end
+
+function autocross = build_autocross()
+
+end

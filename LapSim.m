fd8bac68dd9e1dbcb261f4221208718ab903773d
jvasilak <jvasilak@nd.edu>
2022-01-14 13:50:59 -0500

    Lapsim Comments

--- LapSim.m
+++ LapSim.m
@@ -1,6 +1,34 @@
+%% Clear command window and memory
 clear
 clc
+%% Define Parameters
+% All the variables that may be tweaked for scripting will be defined
+% here
 
-a = aero(12);
-drag = a.find_drag();
-acc = straight(75);
+car_mass = 1000; % in kilograms
+wheel_base = 2; % in meters
+aero_surface_area = 8; % in meters
+
+%% Initialize Lapsim
+% Declare all the variables needed to set up the car, tracks and scoring
+% system
+
+%acceleration = build_acceleration_function(75);
+%skidpad = build_skidpad_function(parameters);
+%endurance = build_endurance_function(parameters);
+%autocross = build_autocross_function(parameters);
+
+%% Run Lap Simulations
+% Calls the functions that simulate the running of each lap
+
+
+%% Calculate Scores for Each Lap
+% Finds the scores each lap would score and adds them up for a total
+% score
+
+
+%% Display Results
+% print the times of each lap in the command window, as well as the
+% associated scores for each one and the total score. Then display our
+% performance visually to better understand the scores.
+
